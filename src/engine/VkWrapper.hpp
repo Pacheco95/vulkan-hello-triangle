3688677310f78889b1784cc0d8af771664215fda
Michael Pacheco <mdpgd95@gmail.com>
2023-11-12 13:39:37 -0300

    feat: add move constructor to VkWrapper

--- src/engine/VkWrapper.hpp
+++ src/engine/VkWrapper.hpp
@@ -33,9 +33,16 @@ class VkWrapper {
     SPDLOG_DEBUG("Created {}: {}", typeid(T).name(), fmt::ptr(m_handle));
   }
 
+  VkWrapper(VkWrapper&& o) noexcept
+      : m_device(std::exchange(o.m_device, VK_NULL_HANDLE)),
+        m_handle(std::exchange(o.m_handle, VK_NULL_HANDLE)),
+        m_allocator(std::exchange(o.m_allocator, VK_NULL_HANDLE)){};
+
   virtual ~VkWrapper() {
-    VkDestroy(m_device, m_handle, m_allocator);
-    SPDLOG_DEBUG("Destroyed {}: {}", typeid(T).name(), fmt::ptr(m_handle));
+    if (m_handle) {
+      VkDestroy(m_device, m_handle, m_allocator);
+      SPDLOG_DEBUG("Destroyed {}: {}", typeid(T).name(), fmt::ptr(m_handle));
+    }
   }
 
   const T& getHandle() const { return m_handle; }
